81a57a2308e56f5f59e7b029ed02387bf5e354b4
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-10-21 08:23:06 +0700

    masih seperti yang di kelas

---
+++ app/src/main/java/com/example/pertemuan4/CobaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.pertemuan4
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan4.Data.DataForm
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class CobaViewModel: ViewModel() {
+    var namaUsr : String by mutableStateOf("")
+        private set
+    var notlp : String by mutableStateOf("")
+        private set
+    var jeniskl : String by mutableStateOf("")
+        private set
+    private val _uiState = MutableStateFlow(DataForm())
+    val uiState: StateFlow<DataForm> = _uiState.asStateFlow()
+
+    fun BacaData(nm:String, tlp:String, jk:String){
+        namaUsr=nm;
+        notlp=tlp;
+        jeniskl=jk;
+    }
+
+    fun setJenis(pilihJK:String){
+        _uiState.update { currentState -> currentState.copy(sex = pilihJK) }
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan4/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.pertemuan4
+
+import android.os.Bundle
+import android.provider.SyncStateContract.Columns
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan4.Data.DataForm
+import com.example.pertemuan4.ui.theme.Pertemuan4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Pertemuan4Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TampilLayar()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TampilLayar(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment =Alignment.CenterHorizontally
+    ) {
+        TampilText()
+
+
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilText(cobaViewModel: CobaViewModel = viewModel()) {
+    var textNama by remember {
+        mutableStateOf("")
+    }
+    var texttlp by remember {
+        mutableStateOf("")
+    }
+
+    val context = LocalContext.current
+    val dataForm : DataForm
+    val uiState by cobaViewModel.uiState.collectAsState()
+    dataForm = uiState;
+
+    Column() {
+        OutlinedTextField(
+            value = textNama,
+            onValueChange ={textNama = it},
+            label = { Text(text = "Nama Lengkap")},
+            modifier = Modifier.padding(10.dp),
+            placeholder = { Text(text = "Nadiv Nugraha")}
+        )
+        OutlinedTextField(
+            value = texttlp,
+            onValueChange ={texttlp = it},
+            label = { Text(text = "Nomor Telepon")},
+            modifier = Modifier.padding(10.dp)
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+
+        }
+
+        TextHasil(namanya = "Andi Harun", telponya ="084973846" , jenisnya ="Bencong" )
+
+    }
+}
+
+@Composable
+fun Jenisbox(){
+
+}
+
+@Composable
+fun TextHasil(namanya: String, telponya: String, jenisnya:String){
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = Modifier.size(width = 300.dp, height = 100.dp)
+    ) {
+        Text(text = "Nama : " + namanya,
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 4.dp)
+        )
+        Text(text = "Telepon : " + telponya,
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 4.dp)
+        )
+        Text(text = "Jenis Kelamin : " + jenisnya,
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 4.dp)
+        )
+
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pertemuan4Theme {
+        TampilLayar()
+    }
+}
